1b71e91cc27bf9317f1a59665880ecbe05daaedf
SystemX <k.yanushevskiy@gmail.com>
2024-09-03 20:38:35 +0400

    ok

---
+++ src/main/java/org/stepandev/razjebalovo/RazjebalovoApplication.java
@@ -0,0 +1,13 @@
+package org.stepandev.razjebalovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazjebalovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazjebalovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/stepandev/razjebalovo/RazjebalovoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stepandev.razjebalovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazjebalovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
